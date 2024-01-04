d0fee706866fdf2dcd178ccf1679dcc8da7ae430
Peter Sukhenko <7243796+pezzu@users.noreply.github.com>
2024-01-03 19:05:44 -0500

    Even quieter apt

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ function prepare_machine() {
     sudo apt -q -qq install -y python3 python3-pip python3-venv
 
     echo "Installing Ansible"
-    sudo apt-add-repository --yes --update ppa:ansible/ansible
+    sudo apt-add-repository --yes --no-update ppa:ansible/ansible
     sudo apt -q -qq install -y ansible
 }
 
